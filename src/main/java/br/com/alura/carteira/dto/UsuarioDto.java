ae2a412052ac1eca8b7bab0f751eac66a5d0ef76
ralf.guth <rsguth@gmail.com>
2021-09-24 20:02:43 -0300

    feat: cria dto para cadastro de usuarios

--- src/main/java/br/com/alura/carteira/dto/UsuarioDto.java
+++ src/main/java/br/com/alura/carteira/dto/UsuarioDto.java
@@ -3,10 +3,11 @@ package br.com.alura.carteira.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 public class UsuarioDto {
 
 	private String nome;
 	private String login;
-	
+
 }
