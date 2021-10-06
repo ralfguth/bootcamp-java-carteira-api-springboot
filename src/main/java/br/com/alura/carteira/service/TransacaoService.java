06356e3e4086402e6c200334ff292f1c8fc6ec03
Ralf Guth <rsguth@gmail.com>
2021-10-05 21:17:27 -0300

    fix: corrige o erro de sobrescrever as transacoes

--- src/main/java/br/com/alura/carteira/service/TransacaoService.java
+++ src/main/java/br/com/alura/carteira/service/TransacaoService.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.alura.carteira.dto.TransacaoDto;
 import br.com.alura.carteira.dto.TransacaoFormDto;
@@ -26,8 +27,12 @@ public class TransacaoService {
 		return transacoes.stream().map(usuario -> modelMapper.map(usuario, TransacaoDto.class)).collect(Collectors.toList());
 	}
 
+	@Transactional
 	public void cadastrar(@Valid TransacaoFormDto dto) {
 		Transacao transacao = modelMapper.map(dto, Transacao.class);
+		transacao.setId(null);
+		// o modelmapper esta atribuindo o mesmo id para usuarioId e transacao, 
+		// essa gambiarra remove o id deixando para o banco a responsabilidade;
 		repository.save(transacao);
 	}
 
