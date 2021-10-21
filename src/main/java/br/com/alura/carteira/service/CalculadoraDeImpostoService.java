22d93f8b541094234f89988778137da980377dbc
Ralf Guth <rsguth@gmail.com>
2021-10-20 22:31:22 -0300

    refactor: fatora calculadora de impostos

--- src/main/java/br/com/alura/carteira/service/CalculadoraDeImpostoService.java
+++ src/main/java/br/com/alura/carteira/service/CalculadoraDeImpostoService.java
@@ -11,10 +11,11 @@ public class CalculadoraDeImpostoService {
 	// 15% de imposto para transações do tipo venda com valor superior a R$20.000,00
 	public BigDecimal calcular(Transacao transacao) {
 		BigDecimal valorTransacao = transacao.getPreco().multiply(new BigDecimal(transacao.getQuantidade()));
-		Boolean isCompra = TipoTransacao.COMPRA.equals(transacao.getTipo());
-		Boolean valorMenorQue20Mil = valorTransacao.compareTo(new BigDecimal("20000")) < 0;
-		
-		return isCompra || valorMenorQue20Mil ? BigDecimal.ZERO : valorTransacao.multiply(new BigDecimal("0.15")).setScale(2, RoundingMode.HALF_UP);
+		if (TipoTransacao.COMPRA.equals(transacao.getTipo()) || valorTransacao.compareTo(new BigDecimal("20000")) < 0) {
+			return BigDecimal.ZERO;
+		} else {
+			return valorTransacao.multiply(new BigDecimal("0.15")).setScale(2, RoundingMode.HALF_UP);
+		}
 	}
 
 }
